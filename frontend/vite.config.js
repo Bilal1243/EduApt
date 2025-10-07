04588cc981f5661f30086fa568803457009bf0b0
Muhammed Bilal <muhammedbilal6211@gmail.com>
2025-10-07 17:21:38 +0530

    changed vite for hosting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server :{
     proxy : {
       '/api' :{
-        target : 'http://localhost:3000/'
+        target : 'https://eduapt-server.onrender.com' //https://eduapt-server.onrender.com  //  http://localhost:3000/
       }
     }
   }
